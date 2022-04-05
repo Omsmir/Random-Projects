186fa6652c53e4aaed2a4b8697472ed3c2ffeca1
OmarSamir <96446413+Omsamiir@users.noreply.github.com>
2022-04-05 07:04:59 +0200

    23TH RANDOM-PROJECT

--- 23-ClassicC/main.js
+++ 23-ClassicC/main.js
@@ -1,8 +1,6 @@
-//Movement Animation to happen
 const card = document.querySelector(".card");
 const container = document.querySelector(".container");
 
-//Items
 const title = document.querySelector(".title");
 const sneaker = document.querySelector(".sneaker img");
 const purchase = document.querySelector(".purchase");
@@ -11,18 +9,16 @@ const sizes = document.querySelector(".sizes");
 const button = document.querySelector(".sizes").querySelectorAll("button")
 
 
-//Moving Animation Event
 container.addEventListener("mousemove", (e) => {
-   let xAxis = (window.innerWidth / 2 - e.pageX) / 25 ;
-   let yAxis = (window.innerHeight / 2 - e.pageY) / 25 ;
+   let xAxis = (window.innerWidth / 2 - e.pageX) / 20 ;
+   let yAxis = (window.innerHeight / 2 - e.pageY) / 20 ;
    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
 
   });
 
 
-//Animate In
 container.addEventListener("mouseenter" , (e) => {
-    card.style.transition = "0.05s ease-in-out"
+    card.style.transition = "none"
 
     
 title.style.transform = "translateZ(50px) scale(1.1)";
@@ -34,7 +30,6 @@ purchase.style.transform = "scale(1.1)"
 })
 
 
-//Animate Out
 container.addEventListener("mouseleave", (e) => {
     card.style.transition = "0.9s ease-in-out";
     card.style.transform = `rotateY(0deg) rotateX(0deg)`;
@@ -47,37 +42,72 @@ container.addEventListener("mouseleave", (e) => {
     purchase.style.transform = "scale(1)";
   });
 
+button.forEach((ele) =>{
+  ele.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("active")){
+      e.target.classList.remove("active")
+    }else {
+      button.forEach((ele) => {
+        ele.classList.remove("active")
+      })
+      e.target.classList.add("active")
+    }
+  })
+})
 
 
+// button[0].addEventListener("click",(ele) =>{
 
-button[0].onclick = function (){
-  button[0].classList.toggle("active")
-  button[1].classList.remove("active")
-  button[2].classList.remove("active")
-  button[3].classList.remove("active")
+//   if(ele.target.classList.contains("active")){
+//     ele.target.classList.remove("active")
+//   }else {
+//     button.forEach((ele) =>{
+//       ele.classList.remove("active")
+//     })
+//     ele.target.classList.add("active")
 
-}
+//   }
 
-button[1].onclick = function (){
-  button[1].classList.toggle("active")
-  button[0].classList.remove("active")
-  button[2].classList.remove("active")
-  button[3].classList.remove("active")
+// })
 
-}
+// button[1].addEventListener("click",(ele) =>{
 
-button[2].onclick = function (){
-  button[2].classList.toggle("active")
-  button[1].classList.remove("active")
-  button[0].classList.remove("active")
-  button[3].classList.remove("active")
+//   if(ele.target.classList.contains("active")){
+//     ele.target.classList.remove("active")
+//   }else {
+//     button.forEach((ele) =>{
+//       ele.classList.remove("active")
+//     })
+//     ele.target.classList.add("active")
 
-}
+//   }
 
-button[3].onclick = function (){
-  button[3].classList.toggle("active")
-  button[1].classList.remove("active")
-  button[2].classList.remove("active")
-  button[0].classList.remove("active")
+// })
 
-}
+// button[2].addEventListener("click",(ele) =>{
+
+//   if(ele.target.classList.contains("active")){
+//     ele.target.classList.remove("active")
+//   }else {
+//     button.forEach((ele) =>{
+//       ele.classList.remove("active")
+//     })
+//     ele.target.classList.add("active")
+
+//   }
+
+// })
+
+// button[3].addEventListener("click",(ele) =>{
+
+//   if(ele.target.classList.contains("active")){
+//     ele.target.classList.remove("active")
+//   }else {
+//     button.forEach((ele) =>{
+//       ele.classList.remove("active")
+//     })
+//     ele.target.classList.add("active")
+
+//   }
+
+// })
