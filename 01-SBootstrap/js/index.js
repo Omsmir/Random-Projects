ce0a0ad88cd83a6b89910b83fe0be30ef8e1308d
OmarSamir <omarsamiir@outlook.com>
2022-04-26 15:05:27 +0200

    update Bootstrap

--- 01-SBootstrap/js/index.js
+++ 01-SBootstrap/js/index.js
@@ -1,13 +1,256 @@
 let li = document.querySelector(".bullets ul").querySelectorAll("li a")
+let h5 = document.querySelector(".bullets ul").querySelectorAll("li a h5")
+let span = document.querySelector(".icon")
+let line = document.querySelector(".icon").querySelectorAll(".icon .line")
+let image = document.querySelector("#projects").querySelectorAll(".image")
+let img = document.querySelector("#projects").querySelectorAll("img")
+let div = document.querySelector("#projects").querySelectorAll(".inner")
+let over = document.querySelector(".overlay-1")
+let Bullets = document.querySelector(".navbar-nav").querySelectorAll("li a")
+let row = document.querySelector("section[id='projects'] .container-fluid .row")
+let btn = document.querySelector(".Main-Btn")
+
+
+
 li.forEach((ele) => {
     ele.onclick = function (){
         if(ele.classList.contains("active")){
             ele.classList.remove("active")
+            li[0].classList.add("active")
+            
         }else {
             li.forEach((ele) => {
                 ele.classList.remove("active")
             })
             ele.classList.add("active")
+
         }
+    if(ele.firstChild.innerText === "DESIGN"){
+        display(ele)
+        defaultAnimate(ele)
+        rowSlow()
+    }else {
+        div.forEach((ele)=> {
+            ele.style.cssText = "opacity: 1; "
+            defaultAnimate(ele)
+
+        })
+        row.style.height = "450px"
+
+
     }
+    if(ele.firstChild.innerText === "PROTOTYPE" ){    
+
+            rowSlow()
+            display(ele)
+            defaultAnimate(ele)
+            
+    } 
+
+    if(ele.firstChild.innerText === "ANDROID"){
+        rowSlow()
+        defaultAnimate(ele)
+        display(ele)
+
+    }
+}
 })
+
+btn.onclick = function (){
+    Bullets.forEach((ele)=>{
+        ele.classList.remove("active")
+    })
+    Bullets[1].classList.add("active")
+
+}
+Bullets.forEach((ele) => {
+    ele.onclick = function (){
+        if(ele.classList.contains("active")){
+            ele.classList.remove("active")
+            
+        }else {
+            Bullets.forEach((ele) => {
+                ele.classList.remove("active")
+            })
+            ele.classList.add("active")
+          }
+        }
+})
+
+
+span.onclick = function (){
+    if(span.classList.contains("co")){
+        line[0].style.cssText = "transform :rotate(45deg); top: 6px; "
+        line[1].style.opacity = "0"
+        line[2].style.cssText = "transform :rotate(-45deg); top: -6px; "
+        span.classList.remove("co")
+       }else {
+        line[0].style.cssText = "transform :rotate(0); top: 0px; "
+            line[1].style.opacity = "1"
+            line[2].style.cssText = "transform :rotate(0deg); top: 0px; "
+            span.classList.add("co")
+           
+       }
+}
+image[0].onclick = function (){
+    over.style.display = "block"
+    let col = document.createElement("div")
+    let image1 = document.createElement("div")
+    let icon = document.createElement("div")
+    let svg = document.createElement("i")
+
+    svg.classList = "fa-solid fa-xmark position-absolute"
+    icon.className = "icon-1"
+    image1.className = "image"
+    col.classList =  "col-sm-8 col-md-8 col-lg-12"
+
+    icon.appendChild(svg)
+
+    image1.appendChild(icon)
+
+    image1.appendChild(img[0])
+
+    col.appendChild(image1)
+
+   over.appendChild(col)
+
+   icon.onclick = function (){
+    over.style.display = "none"
+    image[0].appendChild(img[0])
+   }
+   
+   over.onclick = function (){
+    over.style.display = "none"
+    image[0].appendChild(img[0])
+   }
+}
+
+window.onscroll = function (){
+    if(window.scrollY >= 668){
+        document.querySelector(".navbar").style.cssText = "box-shadow: 1px 2px 15px gray;"
+    }else {
+        document.querySelector(".navbar").style = "box-shadow: none;"
+
+    }
+}
+
+
+
+function animation (ele){
+    ele.animate([
+        {top:"25%",opacity:0},
+        {top: "0",opacity:1}
+    ],{
+        iterations: 1,
+        duration: 600,
+        direction :"alternate",
+  
+    })
+}
+function animationTop1 (ele){
+    ele.animate([
+        {top:"50%",opacity:0},
+        {top: "0",opacity:1}
+    ],{
+        iterations: 1,
+        duration: 600,
+        direction :"alternate",
+  
+    })
+}
+function animation1 (ele){
+    ele.animate([
+        {left:"25%",opacity:0},
+        {left: "0",opacity:1}
+    ],{
+        iterations: 1,
+        duration: 600,
+        direction :"alternate",
+   
+    })
+}
+function TopHeight(ele){
+    ele.animate([
+        {height:"450px"},
+        {height: "200px"}
+    ],{
+        iterations: 1,
+        duration: 800,
+        direction :"alternate",
+   
+    })
+}
+function animation2 (ele){
+    ele.animate([
+        {left:"-25%",opacity:0},
+        {left: "0",opacity:1}
+    ],{
+        iterations: 1,
+        duration: 600,
+        direction :"alternate",
+   
+    })
+}
+
+
+function defaultAnimate(){
+    div[0].addEventListener("animationstart",animation2(div[0]),false)
+    div[1].addEventListener("animationstart",animation1(div[1]),false)
+
+    div[2].addEventListener("animationstart",animation1(div[2]),false)
+    div[3].addEventListener("animationstart",animation(div[3]),false)
+    div[4].addEventListener("animationstart",animationTop1(div[4]),false)
+
+    div[5].addEventListener("animationstart",animation(div[5]),false)
+    div[6].addEventListener("animationstart",animation1(div[6]),false)
+    div[7].addEventListener("animationstart",animationTop1(div[7]),false)
+
+    
+}
+
+function display(ele){
+if(ele.firstChild.innerText === "PROTOTYPE"){
+    div.forEach((ele) => {
+        ele.style.cssText = "opacity: 0; display:none"
+    })
+    div[0].style.opacity = "1"
+    div[0].style.display = "block"
+    div[2].style.opacity = "1"
+    div[2].style.display = "block"
+    div[3].style.opacity = "1"
+    div[3].style.display = "block"
+}
+if(ele.firstChild.innerText === "ANDROID"){
+    div.forEach((el) => {
+        el.style.opacity = "0"
+        el.style.display = "none"
+    })
+    div[4].style.opacity = "1"
+    div[4].style.display = "block"
+    div[6].style.opacity = "1"
+    div[6].style.display = "block"
+    div[7].style.opacity = "1"
+    div[7].style.display = "block"
+}else if(ele.firstChild.innerText === "DESIGN"){
+    div.forEach((ele) => {
+        ele.style.cssText = "opacity: 0; display:none;"
+    })
+    div[1].style.opacity = "1"
+    div[1].style.display = "block"
+
+  div[5].style.opacity = "1"
+  div[5].style.display = "block"
+
+}
+}
+
+function rowSlow(){
+    row.addEventListener("animationstart",TopHeight(row))
+    row.style.height = "200px"
+}
+
+window.onload = function(){
+    window.scrollTo({
+        top:0
+    })
+}
