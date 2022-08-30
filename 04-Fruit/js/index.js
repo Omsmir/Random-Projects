36e43b37981abd4abb63e1af61701ffb7dba6870
OmarSamir <omarsamiir@outlook.com>
2022-08-31 01:32:05 +0200

    New Project

---
+++ 04-Fruit/js/index.js
@@ -0,0 +1,79 @@
+let li = document.querySelectorAll("ul li a")
+let image = document.querySelectorAll("#images .pic img")
+
+let headerImage = document.querySelector("header img")
+
+let section = document.querySelectorAll("section")
+
+
+let minus = document.querySelector(".more .minus")
+let plus = document.querySelector(".more .plus")
+let num = document.querySelector(".more .num")
+
+
+li.forEach((ele) => {
+   ele.addEventListener("click" ,() => {
+    if(ele.classList.contains("active")){
+        return true
+
+    }else {
+        li.forEach((el) => {
+            el.classList.remove("active")
+        })
+        section.forEach((ele) => {
+            if(ele.classList.contains("none")){
+                ele.classList.toggle("none")
+                ele.classList.toggle("visible")
+            }else{
+                ele.classList.toggle("none")
+                section.forEach((el  ) => {
+                    el.classList.remove("visible")
+                })
+            }
+        })
+        ele.classList.add("active")
+    }
+   })
+})
+
+console.log(section)
+
+
+image.forEach((ele) => {
+    ele.addEventListener("click" ,() => {
+        if(ele.classList.contains("active")){
+            return true
+        }else {
+            image.forEach((el) => {
+                el.classList.remove("active")
+            })
+            ele.classList.add("active")
+            headerImage.src = "imgs/" + ele.classList[0] + ".jpg"
+
+        }
+    })
+})
+
+
+minus.addEventListener("click",() => {
+    let n = Number(num.textContent)
+if(n < 10 && n > 0){
+    n--
+}else if(n == 10){
+    n--
+
+}
+num.textContent = n
+})
+
+plus.addEventListener("click",() => {
+    let n = Number(num.textContent)
+    if(n < 10 ){
+        n++
+
+    }else if(n == 10){
+        return false
+    }
+    num.textContent = n
+})
+
--- 25-Preserve3D/Readme.md
+++
@@ -1,4 +0,0 @@
-## Preserve 3D Example 
-<br>
-
-![Preserve3D](https://user-images.githubusercontent.com/96446413/162668227-a3fc417d-a7a5-45ff-8b12-b4bf387dd42f.gif)
