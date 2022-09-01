14ffd5fd45c7b85db860d7df5be4cbe6d0bd2268
OmarSamir <omarsamiir@outlook.com>
2022-09-01 18:38:18 +0200

    New Updates

--- 32-Fruit/js/index.js
+++ 32-Fruit/js/index.js
@@ -1,4 +1,4 @@
-let li = document.querySelectorAll("ul li a")
+let li = document.querySelectorAll("nav ul li a")
 let image = document.querySelectorAll("#images .pic img")
 
 let headerImage = document.querySelector("header img")
@@ -10,6 +10,12 @@ let minus = document.querySelector(".more .minus")
 let plus = document.querySelector(".more .plus")
 let num = document.querySelector(".more .num")
 
+let About = document.getElementById("About")
+
+let nav = document.querySelector(".navbar")
+
+
+let years = document.querySelectorAll(".years ul li a")
 
 li.forEach((ele) => {
    ele.addEventListener("click" ,() => {
@@ -17,15 +23,13 @@ li.forEach((ele) => {
         return true
 
     }else {
-        li.forEach((el) => {
-            el.classList.remove("active")
-        })
+       removeAllActive()
         ele.classList.add("active")
     }
    })
 })
 
-console.log(section)
+
 
 
 image.forEach((ele) => {
@@ -33,9 +37,7 @@ image.forEach((ele) => {
         if(ele.classList.contains("active")){
             return true
         }else {
-            image.forEach((el) => {
-                el.classList.remove("active")
-            })
+            removeAllActive(image)
             ele.classList.add("active")
             headerImage.src = "imgs/" + ele.classList[0] + ".jpg"
 
@@ -65,4 +67,134 @@ plus.addEventListener("click",() => {
     }
     num.textContent = n
 })
+
+
+
+  AOS.init();
+
+console.log(section[2].offsetTop)
+
+ onscroll = function(){
+
+    let position = document.documentElement.scrollTop
+
+        if(position >= section[1].offsetHeight ) {
+                nav.classList.add("back");
+        }else {
+            nav.classList.remove("back")
+        }
+
+  
+
+
+
+    section.forEach((ele) => {
+        if(position >= ele.offsetTop - ele.offsetHeight * .25 && 
+            position < ele.offsetTop + ele.offsetHeight -ele.offsetHeight * .25) {
+                let currentId = ele.attributes.id.value
+
+                removeAllActive(li)
+                addActive(currentId)
+
+            }
+    })
+}
+
+var removeAllActive = function(para){
+    para.forEach((ele) => {
+        ele.classList.remove("active")
+    })
+  }
+
+
+  var addActive = function(id){
+    let select = `.navbar-nav li a[href="#${id}"]`
+
+
+    document.querySelector(select).classList.add("active")
+  }
+
+
+
+
+  let ctx = document.getElementById('myChart')
+  let myChart = new Chart(ctx, {
+      type: 'bar',
+     data: {
+        labels:["Jul","Aug",'Sep',"Oct","Nov","Dec"],
+        datasets: [{
+            label:"Last 6 Months",
+            data: [252,485,1400,2500,785,482]            ,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1,
+            barThickness: 50,
+            borderRadius: 5,
+            hoverBackgroundColor: [
+                "red"
+                ,"green"
+            ]
+
+        }]
+     },
+      options: {
+        animations: {
+            borderColor: {
+              duration: 1000,
+              easing: 'easeInOut',
+              from: 1,
+              to: 5,
+              loop: true
+            }
+          },
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
+  });
+  
+
+    years.forEach((ele) => {
+        ele.addEventListener("click" , () =>{
+            if(ele.classList.contains("active")){
+                GenerateData(ele)
+                
+            }else {
+              removeAllActive(years)
+                ele.classList.add("active")
+                GenerateData(ele)
+            }
+        })
+    })
+
+
+
+function GenerateData(id){
+    let select = id.attributes["data-attr"].value
+    
+    
+    let array = select.split(" ").map((ele) => Number(ele))
+    let data = myChart.config.data;
+
+    data.datasets[0].data = array;  
+    
+    myChart.update()
+ }
+
 
